d2c65fdcad3a147b082a6bec84f775a3fac8470c
Jon Christie <fullstack11235@gmail.com>
2020-12-05 13:07:57 -0500

    Adding CSS & Modules

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -1,7 +1,6 @@
+import React from 'react'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+export default function App ({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
